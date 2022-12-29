5a29d81aac04a4d4b4dff1305c6bb8dd57a0b07b
morgadodesarrollador <morgadodesarrollador@gmail.com>
2022-12-29 13:07:55 +0100

    03: 5. HomePage v1

--- 03-tesloshop/pages/index.tsx
+++ 03-tesloshop/pages/index.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from 'next'
-import { Typography } from '@mui/material';
+import { CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
 import { ShopLayouts } from '../components/layouts'
+import { initialData } from '../database/products';
+import { ProductionQuantityLimits } from '@mui/icons-material';
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +13,21 @@ const Home: NextPage = () => {
         { /* se instanciará al objeto children de ShopLayouts.tsx */}
         <Typography variant='h1' component='h1'>Tienda</Typography>
         <Typography variant='h2' sx = {{ marginBottom: 1 }}>Todos los productos</Typography>
+
+        <Grid container spacing={4}>
+          {
+            initialData.products.map ( product => (
+              <Grid item xs={6} sm= {4} key={ product.slug }>
+                <CardActionArea>
+                  <CardMedia 
+                    component='img'
+                    image={ `products/${product.images[0] }`} 
+                    alt = { product.title }/>
+                </CardActionArea>
+              </Grid>
+            ))
+          }
+        </Grid>
       </ShopLayouts>
   )
 }
