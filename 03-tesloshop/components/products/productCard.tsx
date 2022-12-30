364541458cb3b3f8ff6be240c587163a873a314f
morgadodesarrollador <morgadodesarrollador@gmail.com>
2022-12-30 15:01:55 +0100

    03: 7. Component: ProductCard

--- 03-tesloshop/components/products/productCard.tsx
+++ 03-tesloshop/components/products/productCard.tsx
@@ -1,22 +1,43 @@
-import { Card, CardActionArea, CardMedia, Grid } from "@mui/material"
-import { FC } from "react";
+import { Box, Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import { FC, useState, useMemo } from "react";
 import { IProducto } from '../../interfaces/productos';
 
 interface Props {
     producto: IProducto
 }
 export const ProductCard: FC<Props> = ({ producto }) => {
+ //controlar si el mouse está encima o no. Para cambiar de imagen del producto
+  const [ isHovered, setisHovered ] = useState(false);
+  
+  //deseamos memorizar el productImage
+  const productoImage = useMemo(() => {
+      return isHovered
+        ? `products/${ producto.images[1] }`
+        : `products/${ producto.images[0] }`
+
+  }, [isHovered, producto.images]);
+
   return (
-    <Grid item xs={6} sm= {4} key={ producto.slug }>
+    <Grid item 
+          xs={6} sm= {4}
+          onMouseEnter = { () => setisHovered(true) }
+          onMouseLeave = { () => setisHovered(false) }>
       <Card>
         <CardActionArea>
             <CardMedia 
                 component='img'
-                image={ `products/${producto.images[0] }`} 
+                className='fadeIn'
+                image={ productoImage } 
                 alt = { producto.title }/>
         </CardActionArea>
       </Card>
+      <Box sx= {{ marginTop: 1 }} className='fadeIn'>
+        <Typography fontWeight={700}>{ producto.title }</Typography>
+        <Typography fontWeight={500}>{ `$${producto.price}` }</Typography>
+      </Box>
     </Grid>
   )
 }
+
+
 
